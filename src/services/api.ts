bf944c67b5d5bfa0e73f59bd5df912db9bb82d06
Natan Tavares Santana <64803935+Neitan2001@users.noreply.github.com>
2022-12-15 19:45:30 -0300

    #1 Adição de Interceptor para request (#1)
    * #1 Adição do interceptor para colocar header de autorização em todas as requests
    
    * #1 correção da url do server

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,11 @@ const api = axios.create({
     baseURL: 'https://pionira-backend.onrender.com'
 });
 
+api.interceptors.request.use(function (config) {
+    const token = sessionStorage.getItem('@pionira/token');
+    config.headers['authorization'] = token;
+    
+    return config;
+});
+
 export default api;
