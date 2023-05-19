b28588bcb9cab9b8163dece340fa2fbcd0b378b1
Natan Tavares Santana <64803935+Neitan2001@users.noreply.github.com>
2023-05-19 00:55:58 -0300

    feat(mensagem_de_espera): refatoração do modal de espera (#36)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,8 @@
 export class Constants {
     static FINAL_QUIZ_SINK = 180;
+    static HOURGLASS_SINK = 450;
     static FREE_LUNCH_SOURCE = 290;
     static DAILY_SOURCE = 5;
     static CHEST_LUCK_SOURCE = 15;
-    static CHEST_NORMAL_SOURCE = 3;
+    static CHEST_NORMAL_SOURCE = 3; 
 }
