1ed0533c7dc5289a47f49f671c78df149458eaab
Natan Santana <natan.santana.dev@gmail.com>
2023-06-05 17:09:19 -0300

    feat(ajuste_narrativa): diminuindo numero de falas

--- src/utils/scripts/BlackMambaQuiz/BlackMambaBeggining.ts
+++ src/utils/scripts/BlackMambaQuiz/BlackMambaBeggining.ts
@@ -8,15 +8,13 @@ const blackMambaBeggining = async () => {
             name: "Babuíno",
             image: babuino,
             texts: ["Espere jovem, esse lugar me parece familiar… Uma floresta volumosa… com folhas escuras… esse ar intimidador…",
-                "É isso mesmo jovem, era o que eu temia… você encontrou o covil da Mamba Negra!",
-                "Ela dominou todas as habilidades possíveis para conseguir impedir a ignorância…"
+                "É isso mesmo jovem, era o que eu temia… você encontrou o covil da Mamba Negra! Ela dominou todas as habilidades possíveis para conseguir impedir a ignorância…",
             ]
         },
         {
             name: "Mamba Negra",
             image: blackMamba,
-            texts: ["Mas mesmo assim, sozinha eu falhei. Por isso sou a responsável por desafiar diversos viajantes para ver se possuem o necessário para me ajudar nessa batalha",
-                "Tenha em mente que não pegarei leve com você! Então se sentir que não está preparado, siga as outras trilhas até ter “Sabedoria” o suficiente."]
+            texts: ["Mas mesmo assim, sozinha eu falhei. Por isso sou a responsável por desafiar diversos viajantes para ver se possuem o necessário para me ajudar nessa batalha. Tenha em mente que não pegarei leve com você! Então se sentir que não está preparado, siga as outras trilhas até ter “Sabedoria” o suficiente."]
         }
     ];
 
