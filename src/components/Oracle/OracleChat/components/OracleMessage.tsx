173794e95aac9a1d55da486f66d31625be35c144
Natan Tavares Santana <64803935+Neitan2001@users.noreply.github.com>
2024-04-26 07:16:03 -0300

    Adição da integração com Markdown (#109)
    * feat: Adição da integração com Markdown
    
    * feat: correções PR

--- src/components/Oracle/OracleChat/components/OracleMessage.tsx
+++ src/components/Oracle/OracleChat/components/OracleMessage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text } from '@chakra-ui/react';
 import colorPalette from '../../../../styles/colorPalette';
+import Markdown from 'react-markdown'
 
 const OracleMessage = ({
     message,
@@ -20,15 +21,34 @@ const OracleMessage = ({
         <Text
             color={textProps.color}
             alignSelf={textProps.alignSelf}
-            width="100%"
-            maxWidth="297px"
+            width="fit-content"
             height="fit-content"
             background={textProps.background}
             paddingX="12px"
             paddingY="8px"
             borderRadius={textProps.border}
+            sx={{
+                "ul": {
+                    marginTop: "4px",
+                    paddingLeft: "18px",
+                    paddingRight: "18px",
+                },
+                "ol": {
+                    marginTop: "4px",
+                    paddingLeft: "18px",
+                    paddingRight: "18px",
+                    marginBottom: "4px",
+                },
+                "code": {
+                    whiteSpace: 'pre-wrap',
+                    wordWrap: 'break-word',
+                    maxWidth: '100%',
+                }
+            }}
         >
-            {message}
+            <Markdown>
+                {message}
+            </Markdown>
         </Text>
     )
 }
