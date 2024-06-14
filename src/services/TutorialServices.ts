6f13ca2a7c49c6bf9803cc73ed583db2f3d44b33
Gabriel Sabanai Trindade <gabrielsabanaitrindade@gmail.com>
2024-06-14 08:08:19 -0300

    Feat/tutorial content (#133)
    * feat: almost finish tutorial content
    
    * feat: finish tutorial content
    
    * feat: fix pr revision

--- src/services/TutorialServices.ts
+++ src/services/TutorialServices.ts
@@ -5,10 +5,23 @@ export interface ITutorialTopic {
     icon: string;
     index: number
 }
+
+export interface ITutorialContent {
+    name: string;
+    image: string;
+    content: string;
+    index: number;
+    tutorial_topic_name: string;
+}
 class TutorialServices {
     getAllTutorialTopics = async (): Promise<ITutorialTopic[]> => {
         const response = await api.get('/tutorial/topics')
         return response.data;
+    }
+
+    getContentsByTopic = async (topic_name: string): Promise<ITutorialContent[]> => {
+        const response  = await api.get(`tutorial/contents/${topic_name}`);
+        return response.data
     }
 }
 
