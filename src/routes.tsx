9a0af13eabcc1c589a22bdc57d368b6a712cb9e9
Natan Santana <natan.santana.dev@gmail.com>
2024-07-15 15:44:20 -0300

    fix: Comentar rotas inacessíveis

--- src/routes.tsx
+++ src/routes.tsx
@@ -12,8 +12,8 @@ import ResetPassword from './pages/ResetPassword';
 import Shop from './pages/Shop';
 import Inventory from './pages/Inventory';
 import CheetahPath from './pages/CheetahPath';
-import BlackMambaPath from './pages/blackMambaPath';
-import LionPath from './pages/lionPath';
+// import BlackMambaPath from './pages/blackMambaPath';
+// import LionPath from './pages/lionPath';
 import { RecoilRoot } from 'recoil';
 import { Oracle } from './pages/Oracle';
 
@@ -54,8 +54,10 @@ const Routes = () => {
 						<ProtectedRoute path='/inventory' component={Inventory} />
 						<ProtectedRoute path='/mainPage' component={MainPage} />
 						<ProtectedRoute path='/trilha-cheetah' component={CheetahPath} />
-						<ProtectedRoute path='/finalTrail' component={BlackMambaPath} />
-						<ProtectedRoute path='/trilha-leao' component={LionPath} />
+						{/*
+							<ProtectedRoute path='/finalTrail' component={BlackMambaPath} />
+							<ProtectedRoute path='/trilha-leao' component={LionPath} />
+						*/}
 						<ProtectedRoute path='/oracle' component={Oracle} />
 					</Switch>
 				</AuthProvider >
