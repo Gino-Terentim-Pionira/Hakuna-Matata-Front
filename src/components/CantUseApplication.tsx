1e0e65a1a57c947b53059a3c53b2c34978b15d47
Natan Santana <natan.santana.dev@gmail.com>
2024-08-19 16:27:24 -0300

    feat: Atualização da mensagem para usar browser

--- src/components/CantUseApplication.tsx
+++ src/components/CantUseApplication.tsx
@@ -6,7 +6,7 @@ const CantUseApplication = () => {
     return (
         <Center w='100%' h='100vh' bg={colorPalette.primaryColor} textAlign='center' position='absolute' zIndex="100000000">
             <Text w='90%' fontSize='1.5rem' fontWeight='bold'>
-                A resolução da sua tela está muito pequena para utilizar a aplicação, utilize uma resolução maior!
+                Jovem viajante, a jornada pela Pionira é vasta e rica, mas para explorar todos os seus caminhos, você precisará de um computador. Prepare-se e volte com uma tela maior para continuar a aventura!
             </Text>
         </Center>
     )
