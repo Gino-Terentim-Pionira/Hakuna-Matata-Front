4f769dc58612d634edeaffc683200cb7e413b450
Gabriel Sabanai <gabrielsabanaitrindade@gmail.com>
2025-04-18 15:33:17 -0300

    feat: fix narrative and module

--- src/components/modals/SliderModal.tsx
+++ src/components/modals/SliderModal.tsx
@@ -101,6 +101,7 @@ const SliderModal = ({
                                     initial={{ opacity: 1 }}
                                     animate={{ opacity: 1 }}
                                     exit={{ opacity: 0 }}
+                                    style={{overflowY: "auto"}}
                                 >
                                     <SplitText
                                         initial={{ y: '100%' }}
