d55f03751fdd3d6b31299d2b95b3f501e41a86e7
Natan Santana <natan.santana.dev@gmail.com>
2025-05-16 12:49:11 -0300

    fix

--- src/config/prod.ts
+++ src/config/prod.ts
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: 'https://rmmdpfb2zx234yhc5h2yaofbwm0ahbfv.lambda-url.sa-east-1.on.aws',
+    apiUrl: 'https://bh5qunvzpzwtcj6wtydmhptx6i0nykkx.lambda-url.sa-east-1.on.aws/',
 };
 
 export default config;
